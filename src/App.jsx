67c758392364878ff05cd5dc9d79e0b003130c71
infoflorith-dev <infoflorith@gmail.com>
2026-05-24 19:04:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1784,6 +1784,9 @@ ${profile.resident_name}
       <Button onClick={printReport} variant="outline">
         <Printer className="icon-inline" /> Print / PDF rapport
       </Button>
+      <Button onClick={printDbAnalysisReport} variant="outline">
+  <Printer className="icon-inline" /> Print dB analyse
+</Button>
       <Button onClick={exportJSON} variant="outline">
         <Download className="icon-inline" /> Maak back-up (.json)
       </Button>
